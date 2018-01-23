4581bb254d35919005a070559bb7e7b066301640
Jennah Swander <jenn.swander@gmail.com>
2018-01-23 01:19:11 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function sayHiToGrandma(string) {
+  return string.toLowerCase()
+  console.log("I can't hear you!") 
+  
+}
+
+function sayHiToGrandma(string) {
+  return string.toUpperCase()
+  console.log("YES INDEED!")
+  
+}
+
+function sayHiToGrandma(string) {
+  return "I love you, too."
+  console.log("I love you, Grandma.");
+  
+}
+
+function logShout(string) {
+  console.log()
+}
